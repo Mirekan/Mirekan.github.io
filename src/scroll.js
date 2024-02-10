3fe8498272afbd992d67eff1671e970fc4488fb5
Mirekan <softhard215@gmail.com>
2024-02-10 19:58:43 +0700

    this is the first commit in here the update was only a section animation

---
+++ src/scroll.js
@@ -0,0 +1,15 @@
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+
+
+
+const scrollviewfirst = () => {
+    first.scrollIntoView();
+}
+const scrollviewsecond = () => {
+    second.scrollIntoView();
+}
+const scrollviewthird = () => {
+    third.scrollIntoView();
+}
